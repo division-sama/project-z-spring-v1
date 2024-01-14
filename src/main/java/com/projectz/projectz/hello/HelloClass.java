bc04a3807f4de3a1152ff885f75cde30dd5a865f
division-sama <98158934+division-sama@users.noreply.github.com>
2024-01-14 12:29:49 +0530

    Updated code for /

--- src/main/java/com/projectz/projectz/hello/HelloClass.java
+++ src/main/java/com/projectz/projectz/hello/HelloClass.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "http://localhost:8000")
 public class HelloClass {
     
-    @RequestMapping("/hello")
+    @RequestMapping("/")
     public String sayHi() {
         return "HI";
     }
